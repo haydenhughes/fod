9f3a7267d7e67c67ae0092e3faf864af38db7d4d
Hayden Hughes <hayden@foxes.systems>
2020-05-09 12:40:41 +1000

    Refractor food.js

--- js/food.js
+++ js/food.js
@@ -3,13 +3,13 @@ $(document).ready(function() {
   var food_id = 1;
 
   $("#add-food").click(function() {
-    var template = $($("template").prop("content")).find("#food-item").clone();
+    var $template = $($("template").prop("content")).find("#food-item").clone();
     var id = "food-item-" + food_id;
-    template.attr("id", id);
-    template.find(".input").attr("name", id);
-    template.find(".food-delete").data("target", id);
+    $template.attr("id", id);
+    $template.find(".input").attr("name", id);
+    $template.find(".food-delete").data("target", id);
 
-    $("#food-items").append(template);
+    $("#food-items").append($template);
 
     food_id++;
 
