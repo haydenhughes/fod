81290c8ee6e03c4637c8b9b3ff72c29f7a58d434
Hayden Hughes <hayden@foxes.systems>
2020-05-18 20:03:28 +1000

    Fix naming issues

--- src/models/entries/meta_entry.rs
+++ src/models/entries/meta_entry.rs
@@ -8,25 +8,25 @@ use diesel::prelude::*;
 use serde::Serialize;
 
 type AllColumns = (
-    entries::id,
-    entries::user_id,
-    entries::timestamp,
-    entries::comments,
+    meta_entries::id,
+    meta_entries::user_id,
+    meta_entries::timestamp,
+    meta_entries::comments,
 );
 
-pub type All = Select<entries::table, AllColumns>;
+pub type All = Select<meta_entries::table, AllColumns>;
 
-pub type WithID<'a> = Eq<entries::id, &'a i32>;
+pub type WithID<'a> = Eq<meta_entries::id, &'a i32>;
 pub type ByID<'a> = Filter<All, WithID<'a>>;
 
-pub type WithUserID<'a> = Eq<entries::user_id, &'a i32>;
+pub type WithUserID<'a> = Eq<meta_entries::user_id, &'a i32>;
 pub type ByUserID<'a> = Filter<All, WithUserID<'a>>;
 
-pub type WithTimeStamp<'a> = Eq<entries::timestamp, &'a NaiveDateTime>;
+pub type WithTimeStamp<'a> = Eq<meta_entries::timestamp, &'a NaiveDateTime>;
 pub type ByTimeStamp<'a> = Filter<All, WithTimeStamp<'a>>;
 
 #[derive(Queryable, Serialize, Identifiable, Associations, PartialEq, Debug)]
-#[table_name = "entries"]
+#[table_name = "meta_entries"]
 #[belongs_to(User)]
 pub struct MetaEntry {
     pub id: i32,
@@ -37,19 +37,19 @@ pub struct MetaEntry {
 
 impl MetaEntry {
     pub fn with_id(id: &i32) -> WithID {
-        entries::id.eq(id)
+        meta_entries::id.eq(id)
     }
 
     pub fn with_timestamp(timestamp: &NaiveDateTime) -> WithTimeStamp {
-        entries::timestamp.eq(timestamp)
+        meta_entries::timestamp.eq(timestamp)
     }
 
     pub fn with_user_id(id: &i32) -> WithUserID {
-        entries::user_id.eq(id)
+        meta_entries::user_id.eq(id)
     }
 
     pub fn all() -> All {
-        entries::table.select(entries::all_columns)
+        meta_entries::table.select(meta_entries::all_columns)
     }
 
     pub fn by_timestamp(timestamp: &NaiveDateTime) -> ByTimeStamp {
@@ -64,16 +64,16 @@ impl MetaEntry {
         Self::all().filter(Self::with_user_id(id))
     }
 
-    pub fn to_entry(&self, conn: &PgConnection) -> Result<Entry, diesel::result::Error> {
+    pub fn to_entry(self, conn: &PgConnection) -> Result<Entry, diesel::result::Error> {
         let meal_entry = MealEntry::belonging_to(self)
             .first::<MealEntry>(conn)
-            .map(|e| EntryType::Meal(e));
+            .map(|e| Entry::Meal(self, e));
         let sleep_entry = SleepEntry::belonging_to(self)
             .first::<SleepEntry>(conn)
-            .map(|e| EntryType::Sleep(e));
+            .map(|e| Entry::Sleep(self, e));
         let exercise_entry = ExerciseEntry::belonging_to(self)
             .first::<ExerciseEntry>(conn)
-            .map(|e| EntryType::Exercise(e));
+            .map(|e| Entry::Exercise(self, e));
 
         meal_entry.or(sleep_entry).or(exercise_entry)
     }
