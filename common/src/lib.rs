5184af5eb454094bf881f207877dc2e5be2f99c1
Hayden Hughes <hayden@foxes.systems>
2020-06-10 13:40:03 +1000

    Define API response types in common

--- common/src/lib.rs
+++ common/src/lib.rs
@@ -16,8 +16,41 @@ pub struct Session {
 }
 
 #[derive(Serialize, Deserialize)]
-pub struct CreateEntry {
-    pub meal_type: String,
+pub struct MealType {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateMealType {
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Food {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateFood {
+    pub id: i32,
+    pub name: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Entry {
+    pub id: i32,
+    pub meal_type: MealType,
     pub timestamp: NaiveDateTime,
+    pub foods: Vec<Food>,
+    pub comments: Option<String>,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct CreateEntry {
+    pub meal_type: MealType,
+    pub timestamp: NaiveDateTime,
+    pub foods: Vec<Food>,
     pub comments: Option<String>
 }
